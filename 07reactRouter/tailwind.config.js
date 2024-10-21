aaf6e4d1c2a24a6813981da73b25d36bb85a8065
Aarif Saifi <aarifsaifi731@gmail.com>
2024-10-21 23:23:06 +0530

    reactRoters

---
+++ 07reactRouter/tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+
+
+
+// for (var i = 0; i < 5; i++) { setTimeout(function() { console.log(i); }, i * 1000 ); }
+
+
+// • Flatten following array without using array.flat
+// [1,2,[3,4], [5, [6, 7,[8,[9]]]]] into -> [1,2,3,4,5,6,7,8,9]
+ 
+// • Curry function sum(1)(2)(3)() - > should return 6
+ 
+ 
+// What do the following lines output, and why?
+ 
+// console.log(1 < 2 < 3); console.log(3 > 2 > 1);
+ 
+ 
+// for (var i = 0; i < 5; i++) { 	setTimeout(function() { console.log(i); }, i * 1000 ); }
+ 
+
+// C:\Users\aarif\Namaste React\react_advance_practice
